7ac2c29e91d4220993c1b015639087e0e60949c1
Michael Carpenter <malcom2073@gmail.com>
2022-07-02 15:37:41 -0400

    Console: Added timestamps and message processing to the console

--- src/consoletextview.cpp
+++ src/consoletextview.cpp
@@ -1,6 +1,6 @@
 #include "consoletextview.h"
 #include "ui_consoletextview.h"
-
+#include <QDateTime>
 ConsoleTextView::ConsoleTextView(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ConsoleTextView)
@@ -14,5 +14,27 @@ ConsoleTextView::~ConsoleTextView()
 }
 void ConsoleTextView::consoleText(QString text)
 {
-    ui->textBrowser->append(text);
+    QList<QString> textsplit = text.split("`");
+    QDateTime date = QDateTime::currentDateTime();
+    QString formattedTime = date.toString("yyyy/MM/dd hh:mm:ss");
+    for (int i=0;i<textsplit.size()-1;i+=2)
+    {
+        if (textsplit[i] == "msg")
+        {
+            ui->textBrowser->append(formattedTime + " " + textsplit[i+1]);
+        }
+        else if (textsplit[i] == "outpin")
+        {
+            ui->textBrowser->append(formattedTime + " " + textsplit[i+1]);
+        }
+        else if (textsplit[i] == "rusEfiVersion")
+        {
+
+        }
+        else
+        {
+            qDebug() << "Message not handled" << textsplit[i] << textsplit[i+1];
+        }
+    }
+
 }
